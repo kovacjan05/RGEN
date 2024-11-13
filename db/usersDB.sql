3cc24fd9a6598117e05660ba06410397c1e27dc8
kovacjan05 <jankovac05@gmail.com>
2024-11-13 21:06:52 +0100

    zacatek sql
    zatim jenom zkouska spusteni a opraveni v css rozmazani pri loginu

---
+++ db/usersDB.sql
@@ -0,0 +1,8 @@
+--CREATE TABLE users(
+    --id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL
+--);
+
+--INSERT INTO users(id, email, password)
+--VALUES (1,"test@test.com", "123456789")
+
+--SELECT * FROM users;
