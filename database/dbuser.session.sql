03e2d056ade217aff9468b0d5f84dd280614a8f4
kovacjan05 <145443323+kovacjan05@users.noreply.github.com>
2024-11-18 15:28:20 +0100

    nic

--- database/dbuser.session.sql
+++ database/dbuser.session.sql
@@ -3,7 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL
 );
-INSERT INTO users (username, password_hash)
-VALUES ('test', '123456789')
 SELECT *
 FROM users;
